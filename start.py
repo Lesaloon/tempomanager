2133246f94a8a19c997dc975e1a39170888b0609
Lesaloon <47109666+Lesaloon@users.noreply.github.com>
2023-02-10 17:08:57 +0100

    tabs + docker bdd

--- start.py
+++ start.py
@@ -39,115 +39,115 @@ FREQUENCY=10
 DB_NAME="linky"
 INT_MESURE_KEYS = ['BASE', 'IMAX', 'HCHC', 'IINST', 'PAPP', 'ISOUSC', 'ADCO', 'HCHP']
 
-# serial port configuration ( 9600 7N1 )
+# serial port configuration ( 9600 7E1 )
 ser = serial.Serial(
-    port='/dev/ttyUSB0',
-    baudrate=1200,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.SEVENBITS
+	port='/dev/ttyUSB0',
+	baudrate=1200,
+	parity=serial.PARITY_EVEN,
+	stopbits=serial.STOPBITS_ONE,
+	bytesize=serial.SEVENBITS
 )
 
 # if the serial port is not open, open it
 if not ser.isOpen():
-    ser.open()
+	ser.open()
 
 # Connect to the database
 client = InfluxDBClient('localhost', 8086)
 connected = False
 while not connected:
-    try:
-        logging.info("Database %s exists?" % DB_NAME)
-        # if the database does not exist
-        if not {'name': DB_NAME} in client.get_list_database():
-            # create the database
-            logging.info("Database %s creation.." % DB_NAME)
-            client.create_database(DB_NAME)
-            logging.info("Database %s created!" % DB_NAME)
-        # switch to the database
-        client.switch_database(DB_NAME)
-        logging.info("Connected to %s!" % DB_NAME)
-    # if the database is not reachable
-    except requests.exceptions.ConnectionError:
-        logging.info('InfluxDB is not reachable. Waiting 5 seconds to retry.')
-        time.sleep(5)
-    else:
-        connected = True
+	try:
+		logging.info("Database %s exists?" % DB_NAME)
+		# if the database does not exist
+		if not {'name': DB_NAME} in client.get_list_database():
+			# create the database
+			logging.info("Database %s creation.." % DB_NAME)
+			client.create_database(DB_NAME)
+			logging.info("Database %s created!" % DB_NAME)
+		# switch to the database
+		client.switch_database(DB_NAME)
+		logging.info("Connected to %s!" % DB_NAME)
+	# if the database is not reachable
+	except requests.exceptions.ConnectionError:
+		logging.info('InfluxDB is not reachable. Waiting 5 seconds to retry.')
+		time.sleep(5)
+	else:
+		connected = True
 
 def add_measures(measures, time_measure):
-    points = []
-    for measure, value in measures.items():
-        point = {
-            "measurement": measure,
-            "tags": {
-                # identification de la sonde et du compteur
-                "host": "raspberry",
-                "region": "linky"
-            },
-            "time": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
-            "fields": {
-                "value": value
-            }
-        }
-        points.append(point)
+	points = []
+	for measure, value in measures.items():
+		point = {
+			"measurement": measure,
+			"tags": {
+				# identification de la sonde et du compteur
+				"host": "raspberry",
+				"region": "linky"
+			},
+			"time": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
+			"fields": {
+				"value": value
+			}
+		}
+		points.append(point)
 
-    client.write_points(points)
+	client.write_points(points)
 
 
 def verif_checksum(data, checksum):
-    data_unicode = 0
-    for caractere in data:
-        data_unicode += ord(caractere)
-    sum_unicode = (data_unicode & 63) + 32
-    return (checksum == chr(sum_unicode))
+	data_unicode = 0
+	for caractere in data:
+		data_unicode += ord(caractere)
+	sum_unicode = (data_unicode & 63) + 32
+	return (checksum == chr(sum_unicode))
 
 
 # main loop
 with serial.Serial(port='/dev/ttyS0', baudrate=1200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.SEVENBITS, timeout=1) as ser:
 
-    logging.info("Teleinfo is reading on /dev/ttyS0..")
+	logging.info("Teleinfo is reading on /dev/ttyS0..")
 
-    trame = dict()
+	trame = dict()
 
-    # boucle pour partir sur un début de trame
-    line = ser.readline()
-    while b'\x02' not in line:  # recherche du caractère de début de trame
-        line = ser.readline()
+	# boucle pour partir sur un début de trame
+	line = ser.readline()
+	while b'\x02' not in line:  # recherche du caractère de début de trame
+		line = ser.readline()
 
-    # lecture de la première ligne de la première trame
-    line = ser.readline()
+	# lecture de la première ligne de la première trame
+	line = ser.readline()
 
-    while True:
-        line_str = line.decode("utf-8")
-        logging.debug(line)
+	while True:
+		line_str = line.decode("utf-8")
+		logging.debug(line)
 
-        try:
-            # separation sur espace /!\ attention le caractere de controle 0x32 est un espace aussi
-            [key, val, *_] = line_str.split(" ")
+		try:
+			# separation sur espace /!\ attention le caractere de controle 0x32 est un espace aussi
+			[key, val, *_] = line_str.split(" ")
 
-            # supprimer les retours charriot et saut de ligne puis selectionne le caractere
-            # de controle en partant de la fin
-            checksum = (line_str.replace('\x03\x02', ''))[-3:-2]
+			# supprimer les retours charriot et saut de ligne puis selectionne le caractere
+			# de controle en partant de la fin
+			checksum = (line_str.replace('\x03\x02', ''))[-3:-2]
 
-            if verif_checksum(f"{key} {val}", checksum):
-                # creation du champ pour la trame en cours avec cast des valeurs de mesure en "integer"
-                trame[key] = int(val) if key in INT_MESURE_KEYS else val
+			if verif_checksum(f"{key} {val}", checksum):
+				# creation du champ pour la trame en cours avec cast des valeurs de mesure en "integer"
+				trame[key] = int(val) if key in INT_MESURE_KEYS else val
 
-            if b'\x03' in line:  # si caractère de fin dans la ligne, on insère la trame dans influx
-                del trame['ADCO']  # adresse du compteur : confidentiel!
-                time_measure = time.time()
+			if b'\x03' in line:  # si caractère de fin dans la ligne, on insère la trame dans influx
+				del trame['ADCO']  # adresse du compteur : confidentiel!
+				time_measure = time.time()
 
-                # insertion dans influxdb
-                add_measures(trame, time_measure)
+				# insertion dans influxdb
+				add_measures(trame, time_measure)
 
-                # ajout timestamp pour debugger
-                trame["timestamp"] = int(time_measure)
-                logging.debug(trame)
+				# ajout timestamp pour debugger
+				trame["timestamp"] = int(time_measure)
+				logging.debug(trame)
 
-                trame = dict()  # on repart sur une nouvelle trame
-        except Exception as e:
-            logging.error("Exception : %s" % e, exc_info=True)
-            logging.error("%s %s" % (key, val))
-        line = ser.readline()
+				trame = dict()  # on repart sur une nouvelle trame
+		except Exception as e:
+			logging.error("Exception : %s" % e, exc_info=True)
+			logging.error("%s %s" % (key, val))
+		line = ser.readline()
 
 
--- test.py
+++ test.py
@@ -5,31 +5,24 @@ import time
 import datetime
 import json
 
-# serial port configuration ( 1200 7N1 )
+# serial port configuration ( 1200 7E1 )
 ser = serial.Serial(
-    port='/dev/ttyUSB0',
-    baudrate=1200,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.SEVENBITS
+	port='/dev/ttyUSB0',
+	baudrate=1200,
+	parity=serial.PARITY_EVEN,
+	stopbits=serial.STOPBITS_ONE,
+	bytesize=serial.SEVENBITS
 )
 
 # if the serial port is not open, open it
 if not ser.isOpen():
-    ser.open()
+	ser.open()
 
 # read the serial port
 while True:
-    # read the serial port
-    line = ser.readline()
-    # decode the line
-    line = line.decode('utf-8')
-    # remove the \r and \n
-    line = line.replace('\r','').replace('\n','')
-    # split the line
-    line = line.split(' ')
-    # if the line is not empty
-    if line != ['']:
-       print(line)
+	# read the serial port
+	line = ser.readline()
+	# decode the line
+	line = line.decode('utf-8')
 
-    
+	print(line)
